023c045ac6b996e1bbc02fd904762d3cf10bc242
Steinar Vittersø Kaldager <steinarvk@gmail.com>
2011-12-17 00:43:40 +0100

    semi-working borg

--- borg.c
+++ borg.c
@@ -11,6 +11,95 @@ double logp_complement_of_one_in[MAX_ONE_IN];
 
 int borg_move_primitive( struct bilebio *);
 
+double desirability_map[STAGE_HEIGHT][STAGE_WIDTH];
+
+#define JOIN_XY( x, y ) (((y)<<16) | (x))
+#define GET_X( xy ) ((xy) & 0xffff)
+#define GET_Y( xy ) ( ((xy) & 0xffff0000) >> 16 )
+
+void calculate_distances_to( struct bilebio * ctx, int x, int y, int map[STAGE_HEIGHT][STAGE_WIDTH]) {
+    static int q[STAGE_WIDTH*STAGE_HEIGHT];
+    int qs = 0;
+
+    for(int i=0;i<STAGE_WIDTH;i++) for(int j=0;j<STAGE_HEIGHT;j++) {
+        map[j][i] = -1;
+    }
+
+    map[y][x] = 0;
+
+    q[qs++] = JOIN_XY( x, y );
+
+    while( qs > 0 ) {
+        x = GET_X( q[0] );
+        y = GET_Y( q[0] );
+        memmove( &q[0], &q[1], (qs-1) * sizeof q[0] );
+        qs--;
+
+        for(int i=-1;i<=1;i++) for(int j=-1;j<=1;j++) if( i || j ) {
+            int nx = x + i, ny = y + j;
+            if( nx < 0 || ny < 0 || nx >= STAGE_WIDTH || ny >= STAGE_HEIGHT ) continue;
+            int type = ctx->stage[ny][nx].type;
+            if (type != TILE_FLOOR &&
+                type != TILE_REPELLENT &&
+                type != TILE_EXIT &&
+                type != TILE_NECTAR &&
+                type != TILE_PLAYER &&
+                type != TILE_VINE &&
+                type != TILE_EXIT &&
+                type != TILE_FLOWER) continue;
+            int d = map[y][x] + 1;
+            if( (map[ny][nx] < 0) || (map[ny][nx] > d) ) {
+                map[ny][nx] = d;
+                q[qs++] = JOIN_XY( nx, ny );
+            }
+        }
+    }
+}
+
+void add_desirability_from( struct bilebio * ctx, int x, int y, double base ) {
+    int d[STAGE_HEIGHT][STAGE_WIDTH];
+    calculate_distances_to( ctx, x, y, d );
+
+    for(int nx=0;nx<STAGE_WIDTH;nx++) for(int ny=0;ny<STAGE_HEIGHT;ny++) {
+        if( d[ny][nx] < 0 ) continue;
+        double v = base / (double)(1 + d[ny][nx]);
+        if( v > desirability_map[ny][nx] ) {
+            desirability_map[ny][nx] = v;
+        }
+    }
+}
+
+void calculate_desirability( struct bilebio * ctx ) {
+    for(int x=0;x<STAGE_WIDTH;x++) for(int y=0;y<STAGE_HEIGHT;y++) {
+        desirability_map[y][x] = 0;
+    }
+    for(int x=0;x<STAGE_WIDTH;x++) for(int y=0;y<STAGE_HEIGHT;y++) {
+        switch( ctx->stage[y][x].type ) {
+            case TILE_EXIT:
+                add_desirability_from( ctx, x, y, 100.0 );
+                break;
+        }
+    }
+
+    for(int y=0;y<STAGE_HEIGHT;y++) {
+        for(int x=0;x<STAGE_WIDTH;x++) {
+            int ch = ( ((int)tile_display( ctx->stage[y][x] )) & A_CHARTEXT);
+            if( ch == '.' ) {
+                double thr = 60.0;
+                int cch = '9';
+                while( cch != '0' ) {
+                    if( desirability_map[y][x] >= thr ) break;
+                    thr *= 0.6;
+                    cch--;
+                }
+                ch = cch;
+            }
+            fprintf( borg_log, "%c",  ch );
+        }
+        fprintf( borg_log, "\n" );
+    }
+}
+
 void initialize_borg( struct bilebio * real_world ) {
     world = real_world;
 
@@ -102,8 +191,11 @@ void borg_move_candidates( struct bilebio *ctx, int *candidates, int *no_candida
         const int x = ctx->player_x + i, y = ctx->player_y + j; 
         const int key = keys[j+1][i+1];
         struct tile * t = &ctx->stage[y][x];
-        if( is_obstructed( ctx, x, y ) ) continue;
-        if( t->type != 0 || (i == 0 && j == 0) ) continue;
+        if( is_obstructed( ctx, x, y ) && t->type != TILE_EXIT ) continue;
+
+/*      // the "dodger" style avoided stepping on plants
+        if( t->type == TILE_VINE || t->type == TILE_FLOWER ) continue;
+*/
 
         double log_survival = log_survival_at( ctx->stage, x, y );
         if( log_survival > best_log_survival ) {
@@ -147,8 +239,39 @@ int borg_move() {
         fflush( borg_log );
     }
 
-    int rv = candidates[ rand() % no_candidates ];
-    return rv;
+    int xs[sz], ys[sz];
+
+    for(int i=0;i<no_candidates;i++) {
+        int x = world->player_x, y = world->player_y;
+        switch( candidates[i] ) {
+            case 'y': --y; --x; break;
+            case 'k': --y; break;
+            case 'u': --y; ++x; break;
+            case 'b': ++y; --x; break;
+            case 'j': ++y; break;
+            case 'n': ++y; ++x; break;
+            case 'h': --x; break;
+            case 'l': ++x; break;
+        }
+        xs[i] = x;
+        ys[i] = y;
+    }
+
+    calculate_desirability( world );
+
+    double best_desirability = 0;
+    int rv;
+
+    for(int i=0;i<no_candidates;i++) {
+        double des = desirability_map[ ys[i] ][ xs[i] ];
+        fprintf( borg_log, "desirability of %lf [%d,%d] (%c)\n", des, xs[i], ys[i], candidates[i] );
+        if( des > best_desirability ) {
+            best_desirability = des;
+            rv = i;
+        }
+    }
+
+    return candidates[rv];
 }
 
 int borg_move_primitive( struct bilebio * ctx ) {
